2179bf21c8fa39ec2faa5e9012957289c4399ef6
manish295 <79335262+manish295@users.noreply.github.com>
2022-02-04 16:01:57 -0500

    database functions added

---
+++ database.py
@@ -0,0 +1,88 @@
+import psycopg2
+
+class Database:
+
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            host="localhost",
+            database="flashCardDB",
+            user="postgres",
+            password="manish2005" 
+        )
+        self.cur = self.conn.cursor()
+    
+    def handle_user(self, user_name, user_password):
+        try:
+            self.cur.execute(f"SELECT user_id FROM users WHERE name = '{user_name}' AND password = '{user_password}'")
+            results = self.cur.fetchall()
+            if len(results) == 0:
+                print("User does not exist! Adding now")
+                self.add_user(user_name, user_password)
+                return self.handle_user(user_name, user_password)
+
+            else:
+                print("User exists")
+                return results
+
+        except Exception as err:
+            print(err)
+            self.close()
+
+    def add_user(self, user_name, user_password):
+        try:
+            self.cur.execute(f"INSERT INTO users(name, password) VALUES('{user_name}', '{user_password}')")
+            self.conn.commit()
+            
+        except Exception as err:
+            print(err)
+            self.close()
+
+    def add_set(self, user_id, set_name):
+        try:
+            self.cur.execute(f"INSERT INTO sets(user_id, set_name) VALUES('{user_id}', '{set_name}')")
+            self.conn.commit()
+        except Exception as err:
+            print(err)
+            self.close()
+
+    def add_card(self, set_id, question, answer):
+        try:
+            self.cur.execute(f"INSERT INTO cards(set_id, question, answer) VALUES({set_id}, '{question}', '{answer}')")
+            self.conn.commit()
+        
+        except Exception as err:
+            print(err)
+            self.close()
+
+
+    def get_set(self, user_id):
+        try:
+            self.cur.execute(f"SELECT set_id, set_name FROM sets WHERE user_id = {user_id}")
+            results = self.cur.fetchall()
+            if len(results) == 0:
+                return None
+            else:
+                id = results[0][0]
+                name_list = []
+                for name in results:
+                    name_list.append(name[1])
+                return (id, name_list)
+        except Exception as err:
+            print(err)
+            self.close()
+        
+    def get_card(self, set_id):
+        try:
+            self.cur.execute(f"SELECT question, answer FROM cards WHERE set_id = {set_id}")
+            results = self.cur.fetchall()
+            if len(results) == 0:
+                return None
+            else:
+                return results
+
+        except Exception as err:
+            print(err)
+            self.close()
+
+    def close(self):
+        self.conn.close()
